307466278da5059e649766507c556b8410f2e7e5
lamontu <lamontyu@163.com>
2020-07-26 08:23:00 +0800

    Initial Commit

---
+++ FFStreamTool/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FFStreamTool
+//
+//  Created by YuXiaofei on 2020/7/26.
+//  Copyright © 2020 YuXiaofei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FFStreamToolTests/FFStreamToolTests.swift
@@ -0,0 +1,34 @@
+//
+//  FFStreamToolTests.swift
+//  FFStreamToolTests
+//
+//  Created by YuXiaofei on 2020/7/26.
+//  Copyright © 2020 YuXiaofei. All rights reserved.
+//
+
+import XCTest
+@testable import FFStreamTool
+
+class FFStreamToolTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
